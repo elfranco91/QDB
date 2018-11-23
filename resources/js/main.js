6d6ac72a6c09f390f944288b91f0b7c6ffa95964
Cummins <Alan_Cummins@Dell.com>
2018-11-23 17:05:25 +0000

    3

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,3 +6,13 @@ $(document).ready(function () {
    fade: true
  });
 });
+
+
+$('#zoom-box').animate({
+    width:'150px',
+    height:'150px',
+    top:'0',
+    left:'0',
+    'font-size':'50px',
+    'line-height':'300px'
+}, 6000);
