70eae08f8b64525e5841e48c816be9d19a39db1d
elfranco91 <alan_cummins@dell.com>
2019-01-20 15:54:57 +0000

    contact form

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,3 +17,41 @@ $('#zoom-box').animate({
     'font-size':'50px',
     'line-height':'300px'
 }, 6000);
+
+  //prevent sending if button is clicked without proper data
+$(document).ready(function(){
+  $('.submit').click(function (event){
+
+    console.log('clicked Button')
+
+    var email = $('.email').val()
+    var subject = $('.subject').val()
+    var message = $('.message').val()
+    var statusElm = $('.status')
+    statusElm.empty()
+
+    //valadate a proper email address
+
+    if (email.length > 5 && email.includes('@') && email.includes('.')) {
+      statusElm.append('<div>Email is valid</div>')
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Email is not valid</div>')
+    }
+      //valadate subject
+
+      if(subject.length >= 2) {
+        statusElm.append('<div>Subject is valid</div>')
+      } else {
+        event.preventDefault()
+        statusElm.append('<div>Subject is not valid</div>')
+      }
+        //valadate message
+      if(message.length >= 10) {
+        statusElm.append('<div>Message is valid</div>')
+      } else {
+        event.preventDefault()
+        statusElm.append('<div>Message is not valid</div>')
+      }
+  })
+})
